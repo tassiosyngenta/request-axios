c125404a3f08beda0d87b2549c45eda5810294d7
tassiosyngenta <tassio.rocha@syngenta.com>
2021-02-09 14:36:11 -0300

    Created request service

---
+++ request-service.ts
@@ -0,0 +1,27 @@
+import * as Endpoints from './endpoints';
+import axios from './axios';
+import {AxiosRequestConfig, AxiosResponse} from 'axios';
+
+function requestService() {
+  async function get<T, D = {}>(endpoint: Endpoints.Endpoints, data?: D): Promise<AxiosResponse<T>> {
+    try {
+      return await axios.get<T>(endpoint)
+    } catch (e) {
+      throw(e)
+    }
+  }
+
+  async function post<T, D = {}>(endpoint: Endpoints.Endpoints, data?: D): Promise<AxiosResponse<T>> {
+    try {
+      return await axios.post<T>(endpoint, data)
+    } catch(e) {
+      throw e;
+    }
+  }
+
+  return {
+    get, post
+  }
+}
+
+export default requestService();
