6f45746de5fa19600893adb468a3e9d5f1c2197a
tassiosyngenta <tassio.rocha@syngenta.com>
2021-02-09 14:36:33 -0300

    create get actions to test

---
+++ axios-get.ts
@@ -0,0 +1,19 @@
+import { Endpoints } from './endpoints';
+import RequestService from './request-service';
+
+type User = { id: number, name: string }
+
+async function getUsers() {
+  const users = await RequestService.get<User[]>(Endpoints.requestUsers);
+  console.table(users);
+}
+
+async function getUserById(id: number) {
+  const url = Endpoints.requestUserById.replace('{user_id}',id.toString())
+  const user = await RequestService.get<User>(url as Endpoints);
+
+  console.log(user[0])
+}
+
+getUsers()
+getUserById(1);
