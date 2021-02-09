5574dc592d2dbe9887c0c4e08a729c3d0fa7fcaa
tassiosyngenta <tassio.rocha@syngenta.com>
2021-02-09 14:55:43 -0300

    add comments

--- axios.ts
+++ axios.ts
@@ -1,25 +1,45 @@
 import axios from 'axios';
-import getToken from './fake-get-token';
+import * as TokenService from './fake-get-token';
 
 const instance = axios.create({
   baseURL: 'http://localhost:3000/',
   timeout: 2000,
   headers: {
-    'Authorization': getToken()
+    'Authorization': TokenService.getToken()
   }
 });
 
 instance.interceptors.request.use(
   config => {
    console.log("Axios Request", config)
+   /* 
+   if you need intercept and change anything in request, you do it here:
+
+   headers, body or data, everything in request
+   */
+   
    return config
   },
   error => Promise.reject(error)
 )
 
 instance.interceptors.response.use(
-  response => response.data,
-  error => Promise.reject(error)
+  response =>  {
+    /*
+      if you need intercept and change anything in response, you do it here: 
+
+      manipulate status code success returns and create diffrents return to axios
+    */
+
+    return response.data 
+  },
+  error => { 
+    if(error.response.status === 401) {
+      TokenService.refreshToken()
+    }
+    
+    Promise.reject(error)
+  }
 )
 
 export default instance;
