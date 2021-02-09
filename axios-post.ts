49d1fa4d9cc4cfe3e481175696bfe5de167c5dc9
tassiosyngenta <tassio.rocha@syngenta.com>
2021-02-09 14:36:45 -0300

    create post actions to test

---
+++ axios-post.ts
@@ -0,0 +1,21 @@
+import { Endpoints } from './endpoints';
+import RequestService from './request-service';
+
+
+type User = { id: number, name: string }
+
+async function postUser(params: User) {
+  await RequestService.post<void, User>(Endpoints.requestUsers, params);
+}
+
+async function getUserById(id: number) {
+  const url = Endpoints.requestUserById.replace('{user_id}',id.toString())
+  const user = await RequestService.get<User>(url as Endpoints);
+
+  console.log("++++++++ GET USER ++++++++")
+  console.log(user[0])
+  console.log("++++++++ GET USER ++++++++")
+}
+
+postUser({ id: 7, name: 'Exp 7'})
+getUserById(7);
